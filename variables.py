c620f070ae960970cd75c686013f802b0beb48a2
jakobbalbuena0-coder <jakobbalbuena0@gmail.com>
2026-09-12 09:17:40 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,38 @@
+
+# Multiplication
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 * num2
+print("The answer to your query is:", answer)
+
+# Division
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 / num2
+print("The answer to your query is:", answer)
+
+# Floor division (without remainder)
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 // num2
+print("The answer to your query is:", answer)
+
+# Modulo (remainder)
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 % num2
+print("The answer to your query is:", answer)
+
+# Addition
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 + num2
+print("The answer to your query is:", answer)
+
+# Subtraction
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+answer = num1 - num2
+print("The answer to your query is:", answer)
+
+print("Jakob")
